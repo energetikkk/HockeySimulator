8798c41a58cff3abcf0db22faac6f35330448754
energetikkk <alhalouchyk@gmail.com>
2020-04-11 20:10:45 +0300

    Logic for displaying the picture

--- MainFrame.h
+++ MainFrame.h
@@ -7,11 +7,14 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.pngimage.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
 	TButton *Button1;
+	TImage *Image1;
 	void __fastcall Button1Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
