78474f62360069e87ecc9e32ad501b6e2180bd29
Aliaksandr Halouchyk <alhalouchyk@gmail.com>
2020-04-19 20:57:25 +0300

    Initial UI work
    Some UI(User Interface) examples. Hockey stick and hockey puch has been added. Hockey stick rotation logic.

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,23 +1,39 @@
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 #include <vcl.h>
 #pragma hdrstop
 
 #include "MainFrame.h"
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
-//---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
-	: TForm(Owner)
-{
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::Button1Click(TObject *Sender)
-{
-    ShowMessage("You have done this!");
-}
-//---------------------------------------------------------------------------
 
+// ---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner) {
+}
 
+// ---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender) {
+	ShowMessage("You have done this!");
+}
+// ---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender) {
+	int val = StrToInt(Edit1->Text);
+	Edit2->Text = Edit1->Text;
+	int newval = val + 2;
+	Label1->Caption = newval;
+}
+// ---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button3Click(TObject *Sender) {
+	int angle_initial = StrToInt(SpinEdit1->Text);
+	int normalized_angle = angle_initial % 360;
+	int angle = round(normalized_angle / 15.0) * 15;
+	Label4->Caption = "Piscture is displayed for " + IntToStr(angle) + "angle";
+	String picture_path = "../../resources/hockeySticks/hockeyStick" +
+		IntToStr(angle) + ".png";
+	Image2->Picture->LoadFromFile(picture_path);
+}
+// ---------------------------------------------------------------------------
--- MainFrame.h
+++ MainFrame.h
@@ -9,13 +9,27 @@
 #include <Vcl.Forms.hpp>
 #include <Vcl.ExtCtrls.hpp>
 #include <Vcl.Imaging.pngimage.hpp>
+#include <Vcl.Samples.Spin.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
 	TButton *Button1;
 	TImage *Image1;
+	TLabel *Label1;
+	TButton *Button2;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TImage *Image2;
+	TLabel *Label2;
+	TButton *Button3;
+	TSpinEdit *SpinEdit1;
+	TLabel *Label3;
+	TLabel *Label4;
+	TImage *Image3;
 	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
